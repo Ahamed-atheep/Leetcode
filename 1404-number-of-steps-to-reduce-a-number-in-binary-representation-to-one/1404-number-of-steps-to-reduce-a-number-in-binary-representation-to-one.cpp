60df6ee1a9e83ada0d75d8de845f39bdc97b944d
Ahamed atheep <ahamedatheep.k2024aids@sece.ac.in>
2026-02-26 10:06:26 +0530

    Time: 3 ms (17.36%), Space: 8.7 MB (83.16%) - LeetHub

---
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int numSteps(string s) {
+        int count=0;
+        while(s!="1")
+        {
+            if(s.back()=='0')
+            {
+                s.pop_back();
+            }
+            else if(s.back()=='1')
+            {
+                int i=s.size()-1;
+                while(i>=0 && s[i]=='1')
+                {
+                    s[i]='0';
+                    i--;
+                }
+                if(i<0) s.insert(s.begin(),'1');
+                else {
+                    s[i]='1';
+                }
+            }
+            count++;
+        }
+        return count;
+    }
+};
