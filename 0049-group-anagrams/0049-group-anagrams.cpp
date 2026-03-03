a6f37476baea955421c4a67c8076df5185db05ae
Ahamed atheep <ahamedatheep.k2024aids@sece.ac.in>
2026-03-03 20:13:06 +0530

    Time: 11 ms (90.68%), Space: 26 MB (54.75%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>>mp;
+        for(string i:strs)
+        {
+            string temp=i;
+            sort(temp.begin(),temp.end());
+            mp[temp].push_back(i);
+        }
+        vector<vector<string>>ans;
+        for(auto it:mp)
+        {
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
