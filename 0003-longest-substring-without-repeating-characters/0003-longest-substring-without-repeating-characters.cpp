b58caed868387c8603b9951329ecab6c9df9e8e2
Ahamed atheep <ahamedatheep.k2024aids@sece.ac.in>
2026-03-05 21:01:18 +0530

    Time: 20 ms (22.64%), Space: 14.1 MB (34.9%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -3,9 +3,8 @@ public:
     int lengthOfLongestSubstring(string s) {
         unordered_set<char>st;
         int left=0,right=0,maxi=0;
-        for(char i:s)
-        {
-            if(st.find(i)==st.end())
+        while(right<s.size()){
+            if(st.find(s[right])==st.end())
             {
                 st.insert(s[right]);
                 maxi=max(maxi,right-left+1);
