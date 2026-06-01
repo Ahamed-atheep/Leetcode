e003c4750662bac77b437e366160ae7c42154e1c
Ahamed atheep <ahamedatheep.k2024aids@sece.ac.in>
2026-06-01 12:19:35 +0530

    Time: 0 ms (100%), Space: 9.9 MB (79.7%) - LeetHub

--- 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.cpp
+++ 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.cpp
@@ -4,9 +4,9 @@ public:
         int prefixSum=0;
         int mini=0;
         for(int i=0;i<nums.size();i++)
-        {   prefixSum+=nums[i];
+        {  
+            prefixSum+=nums[i];
             mini=min(mini,prefixSum);
-           
         }
         return abs(1-mini);
     }
